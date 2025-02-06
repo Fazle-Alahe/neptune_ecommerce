18ac842b0f11849e987566648d60a4df3ac6df6b
Fazle-Alahe <121937340+Fazle-Alahe@users.noreply.github.com>
2025-02-06 11:47:20 +0600

    5th_november

--- app/Helpers/Main.php
+++ app/Helpers/Main.php
@@ -2,11 +2,44 @@
 
 namespace App\Helpers;
 
-class Main{
-    public static function product(){
-        
+class Main
+{
+    public static function product($img_src)
+    {
+        $html = '<div class="col-lg-3">';
+        $html .= ' <div class="product">';
+        $html .= '  <div class="product-image">';
+        // $html .= '      <a href="#"><img src="{{ asset("uploads/banner/banner5.jpeg") }}" alt=""></a>';
+        $html .= '      <a href="#"><img src="{{ asset($img_src) }}" alt=""></a>';
+        $html .= '      <div class="discount-badge text-center">';
+        $html .= '          <span>-17%</span>';
+        $html .= '      </div>';
+        $html .= '      <div class="category-badge text-center">';
+        $html .= '          <a href="#"><span>Category</span></a>';
+        $html .= '      </div>';
+        $html .= '  </div>';
+        $html .= ' <div class="product-title px-3 mt-3">';
+        $html .= '      <a href="#"><p>WFE-2H2-GDXX-XX</p></a>';
+        $html .= ' </div>';
+        $html .= ' <div class="price d-flex px-3 mb-1"> ';
+        $html .= '      <p><s class="discount-price me-2">&#2547;40000</s></p><p>&nbsp; &#2547;35000</p> ';
+        $html .= ' </div> ';
+        $html .= ' <div class="product-widget mb-3"> ';
+        $html .= '      <div class="row justify-content-center"> ';
+        $html .= '          <div class="col-6 buy-now text-center"> ';
+        $html .= '              <a href="#" class="">Buy Now</a> ';
+        $html .= '          </div> ';
+        $html .= '          <div class="col-2 text-center product_cart me-1"> ';
+        $html .= '              <a href="#"><i class="fa-solid fa-cart-shopping"></i></a> ';
+        $html .= '          </div> ';
+        $html .= '          <div class="col-2 text-center product_cart"> ';
+        $html .= '              <a href="#"><i class="fa-solid fa-heart"></i></a> ';
+        $html .= '          </div> ';
+        $html .= '         </div> ';
+        $html .= '      </div> ';
+        $html .= '  </div> ';
+        $html .= ' </div> ';
+
+        return $html;
     }
-
 }
-
-?>
--- resources/views/frontend/homepage/recent_products.blade.php
+++ resources/views/frontend/homepage/recent_products.blade.php
@@ -2,29 +2,170 @@
 
 <div class="container-fluid">
     <div class="container">
-        <div class="row">
+        <div class="row gy-4">
             <div class="col-lg-3">
-                <div class="card">
+                <div class="product">
                     <div class="product-image">
-                        <img src="{{ asset('uploads/banner/banner1.jpeg') }}" alt="">
+                        <a href="#"><img src="{{ asset('uploads/banner/banner1.jpeg') }}" alt=""></a>
+                        <div class="discount-badge text-center">
+                            <span>-17%</span>
+                        </div>
+                        <div class="category-badge text-center">
+                            <a href="#"><span>Category</span></a>
+                        </div>
                     </div>
-                    <div class="product-title">
-                        <p>Walton fridge</p>
+                    <div class="product-title px-3 mt-3">
+                        <a href="#"><p>WFE-2H2-GDXX-XX</p></a>
                     </div>
-                    <div class="price d-flex">
-                        <p><s>&#2547;40000</s></p><p>&nbsp; &#2547;35000</p>
+                    <div class="price d-flex px-3 mb-1">
+                        <p><s class="discount-price me-2">&#2547;40000</s></p><p>&nbsp; &#2547;35000</p>
                     </div>
-                    <div class="product-widget">
-                        <div class="row">
-                            <div class="col-6">
-                                <a href="#">Buy Now</a>
+                    <div class="product-widget mb-3">
+                        <div class="row justify-content-center">
+                            <div class="col-6 buy-now text-center">
+                                <a href="#" class="">Buy Now</a>
                             </div>
-                            <div class="col-3"><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></div>
-                            <div class="col-3"><a href="#"><i class="fa-solid fa-heart"></i></a></div>
+                            <div class="col-2 text-center product_cart me-1">
+                                <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                            </div>
+                            <div class="col-2 text-center product_cart">
+                                <a href="#"><i class="fa-solid fa-heart"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            
+            <div class="col-lg-3">
+                <div class="product">
+                    <div class="product-image">
+                        <a href="#"><img src="{{ asset('uploads/banner/banner5.jpeg') }}" alt=""></a>
+                        <div class="discount-badge text-center">
+                            <span>-17%</span>
+                        </div>
+                        <div class="category-badge text-center">
+                            <a href="#"><span>Category</span></a>
+                        </div>
+                    </div>
+                    <div class="product-title px-3 mt-3">
+                        <a href="#"><p>WFE-2H2-GDXX-XX</p></a>
+                    </div>
+                    <div class="price d-flex px-3 mb-1">
+                        <p><s class="discount-price me-2">&#2547;40000</s></p><p>&nbsp; &#2547;35000</p>
+                    </div>
+                    <div class="product-widget mb-3">
+                        <div class="row justify-content-center">
+                            <div class="col-6 buy-now text-center">
+                                <a href="#" class="">Buy Now</a>
+                            </div>
+                            <div class="col-2 text-center product_cart me-1">
+                                <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                            </div>
+                            <div class="col-2 text-center product_cart">
+                                <a href="#"><i class="fa-solid fa-heart"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="product">
+                    <div class="product-image">
+                        <a href="#"><img src="{{ asset('uploads/banner/banner4.jpeg') }}" alt=""></a>
+                        <div class="discount-badge text-center">
+                            <span>-17%</span>
+                        </div>
+                        <div class="category-badge text-center">
+                            <a href="#"><span>Category</span></a>
+                        </div>
+                    </div>
+                    <div class="product-title px-3 mt-3">
+                        <a href="#"><p>WFE-2H2-GDXX-XX</p></a>
+                    </div>
+                    <div class="price d-flex px-3 mb-1">
+                        <p><s class="discount-price me-2">&#2547;40000</s></p><p>&nbsp; &#2547;35000</p>
+                    </div>
+                    <div class="product-widget mb-3">
+                        <div class="row justify-content-center">
+                            <div class="col-6 buy-now text-center">
+                                <a href="#" class="">Buy Now</a>
+                            </div>
+                            <div class="col-2 text-center product_cart me-1">
+                                <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                            </div>
+                            <div class="col-2 text-center product_cart">
+                                <a href="#"><i class="fa-solid fa-heart"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="product">
+                    <div class="product-image">
+                        <a href="#"><img src="{{ asset('uploads/banner/banner3.jpeg') }}" alt=""></a>
+                        <div class="discount-badge text-center">
+                            <span>-17%</span>
+                        </div>
+                        <div class="category-badge text-center">
+                            <a href="#"><span>Category</span></a>
+                        </div>
+                    </div>
+                    <div class="product-title px-3 mt-3">
+                        <a href="#"><p>WFE-2H2-GDXX-XX</p></a>
+                    </div>
+                    <div class="price d-flex px-3 mb-1">
+                        <p><s class="discount-price me-2">&#2547;40000</s></p><p>&nbsp; &#2547;35000</p>
+                    </div>
+                    <div class="product-widget mb-3">
+                        <div class="row justify-content-center">
+                            <div class="col-6 buy-now text-center">
+                                <a href="#" class="">Buy Now</a>
+                            </div>
+                            <div class="col-2 text-center product_cart me-1">
+                                <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                            </div>
+                            <div class="col-2 text-center product_cart">
+                                <a href="#"><i class="fa-solid fa-heart"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="product">
+                    <div class="product-image">
+                        <a href="#"><img src="{{ asset('uploads/banner/banner2.jpeg') }}" alt=""></a>
+                        <div class="discount-badge text-center">
+                            <span>-17%</span>
+                        </div>
+                        <div class="category-badge text-center">
+                            <a href="#"><span>Category</span></a>
+                        </div>
+                    </div>
+                    <div class="product-title px-3 mt-3">
+                        <a href="#"><p>WFE-2H2-GDXX-XX</p></a>
+                    </div>
+                    <div class="price d-flex px-3 mb-1">
+                        <p><s class="discount-price me-2">&#2547;40000</s></p><p>&nbsp; &#2547;35000</p>
+                    </div>
+                    <div class="product-widget mb-3">
+                        <div class="row justify-content-center">
+                            <div class="col-6 buy-now text-center">
+                                <a href="#" class="">Buy Now</a>
+                            </div>
+                            <div class="col-2 text-center product_cart me-1">
+                                <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                            </div>
+                            <div class="col-2 text-center product_cart">
+                                <a href="#"><i class="fa-solid fa-heart"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            {!! Main::product("uploads/banner/banner5.jpeg") !!}
         </div>
     </div>
 </div>
