9f3877343e8fe57a23fa01c8d30ecb792c37aeaa
Fazle-Alahe <121937340+Fazle-Alahe@users.noreply.github.com>
2025-02-13 18:03:36 +0600

    13th_february

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +13,6 @@ Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/user/login', [UserController::class, 'user_login'])->name('user.login');
 Route::get('/user/register', [UserController::class, 'user_register'])->name('user.register');
 Route::get('/reset/password', [UserController::class, 'reset_password'])->name('reset.password');
+
+// frontend product
+Route::get('product/details', [ProductController::class, 'product_details'])->name('product.details');
